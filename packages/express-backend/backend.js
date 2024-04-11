15999c790b2e9febf6b4baba4988e559ddb36fdc
tammy-si <sitammy6@gmail.com>
2024-04-10 23:22:01 -0700

    added id getting

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -39,8 +39,32 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+const findUserByName = (name) => {
+  return users["users_list"].filter((user) => user["name"] === name);
+};
+
 app.get("/users", (req, res) => {
-  res.send(users);
+  const name = req.query.name;
+  if (name != undefined) {
+    let result = findUserByName(name);
+    result = { users_list: result };
+    res.send(result);
+  } else {
+    res.send(users);
+  }
+});
+
+const findUserById = (id) =>
+  users["users_list"].find((user) => user["id"] === id);
+
+app.get("/users/:id", (req, res) => {
+  const id = req.params["id"]; //or req.params.id
+  let result = findUserById(id);
+  if (result === undefined) {
+    res.status(404).send("Resource not found.");
+  } else {
+    res.send(result);
+  }
 });
 
 app.listen(port, () => {
