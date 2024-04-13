49e26afbb44109fe1af07b6d391e999c8213f51b
tammy-si <sitammy6@gmail.com>
2024-04-13 01:00:59 -0700

    Did basic setup for linking

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
 const port = 8000;
@@ -33,6 +34,7 @@ const users = {
   ],
 };
 
+app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
