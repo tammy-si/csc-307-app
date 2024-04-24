a40a1428616124bb777e19891dcb0ae199895f58
tammy-si <sitammy6@gmail.com>
2024-04-24 01:14:00 -0700

    got the users to show up atleast

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -1,38 +1,15 @@
 import express from "express";
 import cors from "cors";
+import {
+  addUser,
+  getUsers,
+  findUserById,
+  findUserByName,
+  findUserByJob,
+} from "./models/userservices.js";
 
 const app = express();
 const port = 8000;
-
-const users = {
-  users_list: [
-    {
-      id: "xyz789",
-      name: "Charlie",
-      job: "Janitor",
-    },
-    {
-      id: "abc123",
-      name: "Mac",
-      job: "Bouncer",
-    },
-    {
-      id: "ppp222",
-      name: "Mac",
-      job: "Professor",
-    },
-    {
-      id: "yat999",
-      name: "Dee",
-      job: "Aspring actress",
-    },
-    {
-      id: "zap555",
-      name: "Dennis",
-      job: "Bartender",
-    },
-  ],
-};
 
 app.use(cors());
 app.use(express.json());
@@ -40,16 +17,6 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
-
-const findUserByName = (name) => {
-  return users["users_list"].filter((user) => user["name"] === name);
-};
-
-const findUserByNameAndJob = (name, job) => {
-  return users["users_list"].filter(
-    (user) => user["job"] === job && user["name"] === name
-  );
-};
 
 const idGenerator = () => {
   let id = "";
@@ -67,25 +34,16 @@ const idGenerator = () => {
 app.get("/users", (req, res) => {
   const name = req.query.name;
   const job = req.query.job;
-  let result = null;
-  if (name != undefined) {
-    // add for job check if necessary
-    if (job != undefined) {
-      result = findUserByNameAndJob(name, job);
-    } else {
-      result = findUserByName(name);
-    }
-    result = { users_list: result };
-    res.send(result);
-  } else {
-    res.send(users);
-  }
-});
 
-const addUser = (user) => {
-  users["users_list"].push(user);
-  return user;
-};
+  getUsers(name ? name : undefined, job ? job : undefined)
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((error) => {
+      console.error("Error getting users:", error);
+      res.status(404).send("Error getting users");
+    });
+});
 
 app.post("/users", (req, res) => {
   const userToAdd = req.body;
@@ -95,8 +53,6 @@ app.post("/users", (req, res) => {
   res.status(201);
   res.send(userToAdd);
 });
-const findUserById = (id) =>
-  users["users_list"].find((user) => user["id"] === id);
 
 app.get("/users/:id", (req, res) => {
   const id = req.params["id"]; //or req.params.id
@@ -107,10 +63,6 @@ app.get("/users/:id", (req, res) => {
     res.send(result);
   }
 });
-
-const deleteUserById = (id) => {
-  return users["users_list"].filter((user) => user["id"] !== id);
-};
 
 app.delete("/users/:id", (req, res) => {
   const id = req.params["id"];
